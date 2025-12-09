3c35214d89f5181e857648e45013d02c44976fa8
SBabchukTM <babchuk.serhii@trident-media.agency>
2025-12-09 18:56:03 +0200

    init

---
+++ RogueMerchant/App/RogueMerchantApp.swift
@@ -0,0 +1,17 @@
+//
+//  RogueMerchantApp.swift
+//  RogueMerchant
+//
+//  Created by Serhii Babchuk on 09.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct RogueMerchantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            RMLaunchView()
+        }
+    }
+}
