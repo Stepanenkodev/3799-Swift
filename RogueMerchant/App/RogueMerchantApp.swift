8fa6bcc67947cb8d063cce2bca1966c292cd4d42
SBabchukTM <babchuk.serhii@trident-media.agency>
2025-12-11 16:01:21 +0200

    add grey part app

--- RogueMerchant/App/RogueMerchantApp.swift
+++ RogueMerchant/App/RogueMerchantApp.swift
@@ -9,9 +9,12 @@ import SwiftUI
 
 @main
 struct RogueMerchantApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
         WindowGroup {
-            RMLaunchView()
+            AppEntryPoint()
         }
     }
 }
